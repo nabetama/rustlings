0fd59b6bc18b4c5b0a18cc30abe4600aae4d8601
nabetama <mao.nabeta@gmail.com>
2023-07-12 22:58:28 +0900

    quiz1, ex1, ex2

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,8 +13,6 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
 
@@ -30,4 +28,9 @@ fn verify_test() {
     assert_eq!(80, price2);
     assert_eq!(41, price3);
     assert_eq!(65, price4);
+}
+
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    let price = if quantity > 40 { 1 } else { 2 };
+    quantity * price
 }
