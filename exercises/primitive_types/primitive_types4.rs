9eb1ce1668b038cbeb2eb0ca5d19044aa56d5bb1
nabetama <mao.nabeta@gmail.com>
2023-07-13 23:30:11 +0900

    primitive_types

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -5,13 +5,20 @@
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    let nice_slice = &a[1..4];
+    // let nice_slice = slice_to_vec(&a);
 
     assert_eq!([2, 3, 4], nice_slice)
+}
+
+fn slice_to_vec(a: &[i32]) -> Vec<i32> {
+    let mut v = Vec::new();
+    for i in a {
+        v.push(*i);
+    }
+    v
 }
